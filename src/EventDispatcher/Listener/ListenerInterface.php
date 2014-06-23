e4fc0f7d5eb2e3699ceedc0b020227ad5340dea2
Mike Zukowsky <michal@durooil.com>
2014-06-23 11:31:40 +0200

    Update ListenerInterface.php

--- src/EventDispatcher/Listener/ListenerInterface.php
+++ src/EventDispatcher/Listener/ListenerInterface.php
@@ -5,5 +5,9 @@ use Zend\EventManager\EventInterface;
 
 interface ListenerInterface
 {
+    /**
+     * @var EventInterface|\Zend\Mvc\MvcEvent $event
+     * @return mixed
+     */
     public function onEvent(EventInterface $event);
 }
